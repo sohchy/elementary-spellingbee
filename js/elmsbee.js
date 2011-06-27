76ad85b64e51209dc9001cdc28e84f865085818c
Sourabh Chakraborty <sourabh.chakraborty@gmail.com>
2011-06-26 21:40:12 -0500

    final commit before sleepng. minor changes

--- js/elmsbee.js
+++ js/elmsbee.js
@@ -1,5 +1,34 @@
 $(document).ready(function(){
 
+	$screenKeyboardRow_a = $(".screenKeyboardRow a");
 	
+	$typeWordInputBox_input = $("#typeWordInputBox input");
+	
+	$typeWordInputBox_input.val('');
+
+	$screenKeyboardRow_a.click(function(){
+	
+		var keyEntered = $(this).text();
+		
+		var inputBoxCurrentValue = $typeWordInputBox_input.val();
+		
+		switch(keyEntered) {
+			case 'DELETE':
+				$typeWordInputBox_input.val(inputBoxCurrentValue.substring(0,inputBoxCurrentValue.length-1));
+				break;
+			case 'ENTER':
+				console.log('enter');
+				break;
+			default:
+				$typeWordInputBox_input.val(inputBoxCurrentValue+keyEntered);
+				break;
+		
+		}
+		
+		
+		
+		
+		return false;
+	});
 
 });
